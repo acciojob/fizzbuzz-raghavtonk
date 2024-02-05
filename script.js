fa597db6a27f52d6a6cd989122c69c0e91142db6
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-05 23:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let result =""
+for (let i = 1 ; i<= 100;i++){
+	if(i % 3 == 0 && i % 5 == 0)
+		result += "FizzBuzz" + "\n"
+	
+	else if(i % 3 == 0)
+		result += "Fizz" + "\n"
+	
+	else if(i % 5 == 0)
+		result += "Buzz" + "\n"
+	
+	else
+     result+= i + "\n"
+	
+}
+alert(result); 
